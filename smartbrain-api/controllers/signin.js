e35a182a4e89f8dab71a1cece71e2ed448e507ca
lenindelarosa <leninmanueld@gmail.com>
2022-02-08 18:39:45 -0400

    reviewing the code, cleaner

---
+++ smartbrain-api/controllers/signin.js
@@ -0,0 +1,23 @@
+
+const handleSignin = (db, bcrypt) => (req, res) => {
+    db.select('email', 'hash').from('login')
+        .where('email', '=', req.body.email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            if (isValid){
+                return db.select('*').from('users')
+                    .where('email', '=', req.body.email)
+                    .then(user => {
+                        res.json(user[0])
+                    })
+                    .catch(err => res.status(400).json('Unable to get user.'))
+            } else {
+                res.status(400).json('Wrong username/password combination.')
+            }
+        })
+        .catch(err => res.status(400).json('Wrong username/password combination.'))
+}
+
+module.exports = {
+    handleSignin
+}
--- smartbrain-api/server.js
+++ smartbrain-api/server.js
@@ -3,7 +3,11 @@ const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex')
+
 const register = require('./controllers/register');
+const signin  = require('./controllers/signin');
+const image = require('./controllers/image');
+const profile = require('./controllers/profile');
 
 const db = knex({
     client: 'pg',
@@ -28,52 +32,13 @@ app.get('/', (req, res)=>{
     res.send('Success!');
 })
 
-app.post('/signin', (req, res) => {
-    db.select('email', 'hash').from('login')
-        .where('email', '=', req.body.email)
-        .then(data => {
-            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-            if (isValid){
-                return db.select('*').from('users')
-                    .where('email', '=', req.body.email)
-                    .then(user => {
-                        res.json(user[0])
-                    })
-                    .catch(err => res.status(400).json('Unable to get user.'))
-            } else {
-                res.status(400).json('Wrong username/password combination.')
-            }
-        })
-        .catch(err => res.status(400).json('Wrong username/password combination.'))
-})
+app.post('/signin', signin.handleSignin(db, bcrypt));
 
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    let found = false;
-    db.select('*').from('users').where({id})
-        .then(user => {
-            if(user.length){
-                res.json(user[0])
-            } else {
-                res.status(400).json('Not found')
-            }
-            
-        })
-        .catch(err => res.status(400).json('Error getting user'));
-})
+app.get('/profile/:id', (req, res, db) => {profile.handleProfileGet(req, res, db)})
 
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-        .increment('entries', 1)
-        .returning('entries')
-        .then(entries => {
-            res.json(entries[0].entries)
-        })
-        .catch(err => res.status(400).json('Unable to get entries count.'))
-})
+app.put('/image', (req,res) => { image.handleImage (req, res, db) });
 
 app.listen(3000, () => {
     console.log('App is running on port 3000.');
